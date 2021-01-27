525c7e13bb66f270d2ac16c0e0fca8174f0f1380
Maxim Novikov <maximkanv@gmail.com>
2021-01-27 17:38:08 +0300

    asd

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,3 +1,8 @@
 #!/usr/bin/env node
+import commander from 'commander';
 
-console.log('Aloha!');
+const program = new commander.Command();
+program
+  .version('0.0.1')
+  .description('Compares two configuration files and shows a difference.');
+program.parse(process.argv);
