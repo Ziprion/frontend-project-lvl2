7cdee9852dc7d5376b8262ef22d0ffe6aebc2149
Maxim Novikov <maximkanv@gmail.com>
2021-01-29 16:07:15 +0300

    asd

--- src/gD.js
+++ src/gD.js
@@ -1,20 +1,32 @@
 import fs from 'fs';
 import _ from 'lodash';
 
+const addDifKeys = (file, anotherFile, mark, toArray) => {
+  const arrayOfKeys = _.keysIn(file);
+  arrayOfKeys.forEach((key) => {
+    if (!_.has(anotherFile, key)) {
+      toArray.push(`${key}: ${file[key]}  ${mark} `);
+    }
+  });
+};
+const handlerToFormat = (array) => {
+  const newResult = array.map((item) => {
+    if (_.isArray(item)) {
+      return handlerToFormat(item);
+    }
+    const preString = item.slice(item.length - 4, item.length);
+    const mainString = item.slice(0, item.length - 4);
+    const newString = preString + mainString;
+    return newString;
+  });
+  return newResult;
+};
 export default function genDiff(filepath1, filepath2) {
   const fileOne = JSON.parse(fs.readFileSync(filepath1));
   const fileTwo = JSON.parse(fs.readFileSync(filepath2));
   const result = [];
-  const addDifKeys = (file, anotherFile, mark) => {
-    const arrayOfKeys = _.keysIn(file);
-    arrayOfKeys.forEach((key) => {
-      if (!_.has(anotherFile, key)) {
-        result.push(`${key}: ${file[key]}  ${mark} `);
-      }
-    });
-  };
-  addDifKeys(fileOne, fileTwo, '-');
-  addDifKeys(fileTwo, fileOne, '+');
+  addDifKeys(fileOne, fileTwo, '-', result);
+  addDifKeys(fileTwo, fileOne, '+', result);
   _.keysIn(fileTwo).forEach((key) => {
     if (_.has(fileOne, key)) {
       if (fileOne[key] === fileTwo[key]) {
@@ -24,18 +36,6 @@ export default function genDiff(filepath1, filepath2) {
       }
     }
   });
-  const handlerToFormat = (array) => {
-    const newResult = array.map((item) => {
-      if (_.isArray(item)) {
-        return handlerToFormat(item);
-      }
-      const preString = item.slice(item.length - 4, item.length);
-      const mainString = item.slice(0, item.length - 4);
-      const newString = preString + mainString;
-      return newString;
-    });
-    return newResult;
-  };
   const sortedFormatResult = handlerToFormat(result.sort()).flat().join('\r\n');
   return `\r\n{\r\n${sortedFormatResult}\r\n}\r\n`;
 }
