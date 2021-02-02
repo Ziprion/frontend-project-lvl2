8fd1e5e8d89b86d063230c975823945cd9221916
Maxim Novikov <maximkanv@gmail.com>
2021-02-02 16:36:54 +0300

    asd

--- formatters/index.js
+++ formatters/index.js
@@ -2,7 +2,7 @@ import formatter from './default.js';
 import formatPlain from './plain.js';
 
 function whichformat(prevResult, format) {
-  if (format === 'plain') { return `\n${formatPlain(prevResult).join('')}`; }
+  if (format === 'plain') { return `\r\n${formatPlain(prevResult).join('')}`; }
   if (format === 'json') { return JSON.stringify(prevResult); }
   return `{\n${formatter(prevResult)}}`;
 }
