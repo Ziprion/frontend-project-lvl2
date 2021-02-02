20dc90d71944713a69e7921c6c37402fe042598c
Maxim Novikov <maximkanv@gmail.com>
2021-02-02 17:10:35 +0300

    asd

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -3,7 +3,7 @@ import genDiff from '../src/gD.js';
 
 test('genDiff JSON tree check', () => {
   const expectString = fs.readFileSync('./__fixtures__/fortest2.txt', 'utf8');
-  expect(genDiff('./__fixtures__/file3.json', './__fixtures__/file4.json')).toMatch(expectString);
+  expect(genDiff('./__fixtures__/file3.json', './__fixtures__/file4.json')).toEqual(expectString);
 });
 test('genDiff JSON plain check', () => {
   const expectString = fs.readFileSync('./__fixtures__/fortest3.txt', 'utf8');
--- formatters/default.js
+++ formatters/default.js
@@ -18,15 +18,15 @@ const formatter = (array, inner = 1) => {
     const value = _.isObject(item.value) ? formatter(item.value, inner + 2) : item.value;
     const newValue = _.isObject(item.newValue) ? formatter(item.newValue, inner + 2)
       : item.newValue;
-    const preStr = '{\r\n';
+    const preStr = '{\n';
     const postStr = `${newTab}  }`;
     const resultValue = _.isObject(item.value) ? preStr + value + postStr : value;
     const resultNewValue = _.isObject(item.newValue) ? preStr + newValue + postStr : newValue;
     if (value === newValue) {
-      str += `${newTab}${status} ${item.name}: ${resultValue}\r\n`;
+      str += `${newTab}${status} ${item.name}: ${resultValue}\n`;
     } else {
-      str += `${newTab}- ${item.name}: ${resultValue}\r\n`;
-      str += `${newTab}+ ${item.name}: ${resultNewValue}\r\n`;
+      str += `${newTab}- ${item.name}: ${resultValue}\n`;
+      str += `${newTab}+ ${item.name}: ${resultNewValue}\n`;
     }
     return true;
   });
