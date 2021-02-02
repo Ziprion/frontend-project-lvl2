5fb9d8e709c9a41394e320ae1f34f56278ca9189
Maxim Novikov <maximkanv@gmail.com>
2021-02-02 20:46:34 +0300

    asd

--- formatters/plain.js
+++ formatters/plain.js
@@ -1,44 +1,45 @@
 import _ from 'lodash';
-
-const res = [];
-const formatPlain = (array, path = '') => {
-  array.map((item) => {
-    const newPath = path + item.name;
-    const getValue = (asd) => {
-      if (_.isObject(asd.value)) {
-        return '[complex value]';
+export default function plain(arras) {
+  const res = [];
+  function formatPlain(array, path = '') {
+    array.map((item) => {
+      const newPath = path + item.name;
+      const getValue = (asd) => {
+        if (_.isObject(asd.value)) {
+          return '[complex value]';
+        }
+        if (typeof asd.value === 'string') {
+          return `'${asd.value}'`;
+        }
+        return `${asd.value}`;
+      };
+      const getNewValue = (asd) => {
+        if (_.isObject(asd.newValue)) {
+          return '[complex value]';
+        }
+        if (typeof asd.newValue === 'string') {
+          return `'${asd.newValue}'`;
+        }
+        return `${asd.newValue}`;
+      };
+      const value = getValue(item);
+      const newValue = getNewValue(item);
+      if (item.status === 'deleted') {
+        res.push(`Property '${newPath}' was removed`);
       }
-      if (typeof asd.value === 'string') {
-        return `'${asd.value}'`;
+      if (item.status === 'added') {
+        res.push(`Property '${newPath}' was added with value: ${newValue}`);
       }
-      return `${asd.value}`;
-    };
-    const getNewValue = (asd) => {
-      if (_.isObject(asd.newValue)) {
-        return '[complex value]';
+      if (item.status === 'changed') {
+        res.push(`Property '${newPath}' was updated. From ${value} to ${newValue}`);
       }
-      if (typeof asd.newValue === 'string') {
-        return `'${asd.newValue}'`;
+      if (_.isObject(item.newValue)) {
+        const trigPath = '.';
+        return formatPlain(item.newValue, newPath + trigPath);
       }
-      return `${asd.newValue}`;
-    };
-    const value = getValue(item);
-    const newValue = getNewValue(item);
-    if (item.status === 'deleted') {
-      res.push(`Property '${newPath}' was removed`);
-    }
-    if (item.status === 'added') {
-      res.push(`Property '${newPath}' was added with value: ${newValue}`);
-    }
-    if (item.status === 'changed') {
-      res.push(`Property '${newPath}' was updated. From ${value} to ${newValue}`);
-    }
-    if (_.isObject(item.newValue)) {
-      const trigPath = '.';
-      return formatPlain(item.newValue, newPath + trigPath);
-    }
-    return true;
-  });
-  return res;
-};
-export default formatPlain;
+      return true;
+    });
+    return res;
+  };
+  return formatPlain(arras);
+}
