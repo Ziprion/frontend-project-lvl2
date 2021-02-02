ac64ff8c6bb30f3a9413c51876fbf4724a27a373
Maxim Novikov <maximkanv@gmail.com>
2021-02-02 17:51:38 +0300

    asd

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -1,11 +1,69 @@
-import fs from 'fs';
 import genDiff from '../src/gD.js';
 
+
+const strPlain = `
+Property 'common.follow' was added with value: false
+Property 'common.setting2' was removed
+Property 'common.setting3' was updated. From true to null
+Property 'common.setting4' was added with value: 'blah blah'
+Property 'common.setting5' was added with value: [complex value]
+Property 'common.setting6.doge.wow' was updated. From '' to 'so much'
+Property 'common.setting6.ops' was added with value: 'vops'
+Property 'group1.baz' was updated. From 'bas' to 'bars'
+Property 'group1.nest' was updated. From [complex value] to 'str'
+Property 'group2' was removed
+Property 'group3' was added with value: [complex value]
+`;
+
+const strStylish = `
+{
+    common: {
+      + follow: false
+        setting1: Value 1
+      - setting2: 200
+      - setting3: true
+      + setting3: null
+      + setting4: blah blah
+      + setting5: {
+            key5: value5
+        }
+        setting6: {
+            doge: {
+              - wow: 
+              + wow: so much
+            }
+            key: value
+          + ops: vops
+        }
+    }
+    group1: {
+      - baz: bas
+      + baz: bars
+        foo: bar
+      - nest: {
+            key: value
+        }
+      + nest: str
+    }
+  - group2: {
+        abc: 12345
+        deep: {
+            id: 45
+        }
+    }
+  + group3: {
+        deep: {
+            id: {
+                number: 45
+            }
+        }
+        fee: 100500
+    }
+}
+`;
 test('genDiff JSON tree check', () => {
-  const expectString = fs.readFileSync('./__fixtures__/fortest2.odt', 'utf8');
-  expect(genDiff('./__fixtures__/file3.json', './__fixtures__/file4.json')).toEqual(expectString);
+  expect(genDiff('./__fixtures__/file3.json', './__fixtures__/file4.json')).toEqual(strStylish);
 });
 test('genDiff JSON plain check', () => {
-  const expectString = fs.readFileSync('./__fixtures__/fortest2.txt', 'utf8');
-  expect(genDiff('./__fixtures__/file3.json', './__fixtures__/file4.json', 'plain')).toEqual(expectString);
+  expect(genDiff('./__fixtures__/file3.json', './__fixtures__/file4.json', 'plain')).toEqual(strPlain);
 });
