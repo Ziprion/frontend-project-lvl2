487345b9babf9712d26b8afa3a549bbb67f8362c
Maxim Novikov <maximkanv@gmail.com>
2021-02-02 20:11:15 +0300

    asd

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import diffObj from './src/gD.js';
 import whichformat from './formatters/index.js';
 import parser from './src/parsers.js';
 
-export default function genDif(filepath1, filepath2, format = 'default') {
+export default function genDiff(filepath1, filepath2, format = 'default') {
   const fileOne = parser(filepath1);
   const fileTwo = parser(filepath2);
   const result = diffObj(fileOne, fileTwo);
