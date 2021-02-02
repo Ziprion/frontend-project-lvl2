2dbfe36f01f6e3d8803477a7811ce0dad6216cb6
Maxim Novikov <maximkanv@gmail.com>
2021-02-02 17:37:04 +0300

    asd

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -2,10 +2,10 @@ import fs from 'fs';
 import genDiff from '../src/gD.js';
 
 test('genDiff JSON tree check', () => {
-  const expectString = fs.readFileSync('./__fixtures__/fortest2.txt', 'utf8');
+  const expectString = fs.readFileSync('./__fixtures__/fortest2.odt', 'utf8');
   expect(genDiff('./__fixtures__/file3.json', './__fixtures__/file4.json')).toEqual(expectString);
 });
 test('genDiff JSON plain check', () => {
-  const expectString = fs.readFileSync('./__fixtures__/fortest3.txt', 'utf8');
+  const expectString = fs.readFileSync('./__fixtures__/fortest2.txt', 'utf8');
   expect(genDiff('./__fixtures__/file3.json', './__fixtures__/file4.json', 'plain')).toEqual(expectString);
 });
