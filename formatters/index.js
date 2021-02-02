7a96144a950a5ed23f0fddd644cdda5be94e1095
Maxim Novikov <maximkanv@gmail.com>
2021-02-02 16:41:04 +0300

    asd

--- formatters/index.js
+++ formatters/index.js
@@ -4,6 +4,6 @@ import formatPlain from './plain.js';
 function whichformat(prevResult, format) {
   if (format === 'plain') { return `\r\n${formatPlain(prevResult).join('')}`; }
   if (format === 'json') { return JSON.stringify(prevResult); }
-  return `{\n${formatter(prevResult)}}`;
+  return `{\r\n${formatter(prevResult)}}\r\n`;
 }
 export default whichformat;
